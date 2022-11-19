bdf21ec5c64b6475152d7b327907f132f8631337
Holly Tippins <hollytpns@gmail.com>
2022-11-19 16:09:56 +0000

    changed hero page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,9 +2,9 @@
     <the-header></the-header>
     <section>
         <div
-            class="flex flex-col font-serif md:flex-row md:p-4 md:justify-around md:py-36"
+            class="flex flex-col font-serif md:flex-row md:justify-end md:content-center w-full pt-36"
         >
-            <div class="p-6 md:p-2 md:w-1/4">
+            <div class="p-6 md:p-2 md:w-1/2 md:pl-16">
                 <p class="text-sm md:text-lg pb-2">Hello, my name is Holly</p>
                 <h1 class="text-2xl md:text-5xl">
                     I'm an aspiring
@@ -12,12 +12,24 @@
                     based in the United Kingdom.
                 </h1>
             </div>
-            <div class="pr-24 self-end">
-                <!-- <img
+        </div>
+
+        <!-- <img
                     src="../assets/self.jpg"
                     alt="b&w photo of author, smiling"
                     class="rounded-full w-64 h-64 md:w-72 md:h-72"
                 /> -->
+        <div class="relative w-full h-60 mt-44">
+            <div class="absolute bottom-0 left-0 w-1/2">
+                <img
+                    src="../assets/tall-building.jpg"
+                    alt=""
+                    class="object-contain"
+                />
+                <!-- <p>
+                    Lancashire based front-end web developer focusing on
+                    creating clean, user-focused experiences
+                </p> -->
             </div>
         </div>
     </section>
